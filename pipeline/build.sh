8abe4c82ab0958d937b8cfc8d3397ac78f577369
Yorke Rhodes IV <Yorke.Rhodes.IV@ibm.com>
2018-06-19 14:56:37 -0400

    Add PREPARE stage to pipeline
    Signed-off-by: Yorke Rhodes IV <yorke.rhodes.iv@ibm.com>

--- pipeline/build.sh
+++ pipeline/build.sh
@@ -6,15 +6,6 @@ echo "######## Build chaincode ########"
 mkdir build
 cd build
 
-# Download and install go binaries in current directory 
-# This script also sets the GOPATH and GOROOT variables used later in this script
-. ../pipeline/install-go.sh
-
-#Download Hyperledger Fabric src in current directory 
-# This script also set the FABRIC_SRC_DIR variable used later in this script
-. ../pipeline/download-fabric.sh
-
-
 echo "######## Placing source in directory expected by go build ########"
 mkdir ${GOPATH}/src
 # Let's put fabric source into gopath so that go can resolve dependencies with Fabric libraries 
---
+++ pipeline/prepare.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+default_scripts=
+"build.sh
+deploy-enterprise.sh
+download-fabric.sh
+install-go.sh
+unitest.sh"
+
+if [ ! -f  ${SCRIPT_DIR:=./scripts/} ]; then
+  mkdir -p ${SCRIPT_DIR}
+fi
+
+for script in ${BUILD_SCRIPTS:=$default_scripts}; do
+  SCRIPT_SRC="${SCRIPT_URL:=https://raw.githubusercontent.com/yorhodes/build-lib/fabric/pipeline}/${script}"
+  SCRIPT_FILE="${SCRIPT_DIR}${script}"
+  
+  if [ ! -f  ${SCRIPT_FILE} ]; then
+    echo -e "No script found at ${SCRIPT_FILE}, defaulting to ${SCRIPT_SRC}"
+    (curl -sSL ${SCRIPT_SRC}) > ${SCRIPT_FILE}
+  else
+    echo "Script found at ${SCRIPT_FILE}"
+  fi
+done
