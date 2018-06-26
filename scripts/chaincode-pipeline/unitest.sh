dda773d7e5894ddacc28b7bef7d23227fbdc8395
Yorke Rhodes IV <Yorke.Rhodes.IV@ibm.com>
2018-06-26 11:30:03 -0400

    Fix build directory usage in pipeline stages (prepare/build/unitest)
    Signed-off-by: Yorke Rhodes IV <yorke.rhodes.iv@ibm.com>

--- scripts/chaincode-pipeline/unitest.sh
+++ scripts/chaincode-pipeline/unitest.sh
@@ -1,12 +1,6 @@
 #!/bin/bash -x
 echo "######## Unitest chaincode ########"
-ls -la
-cd build
-#assumption is that previous state copied go code under build directory
-export GOPATH=$(pwd)
-# assumption is that previous state installed go binaries in build directory 
-export GOROOT=$(pwd)/go
-export PATH=${GOROOT}/bin:$PATH
+source ./scripts/chaincode-pipeline/env.sh
 
 #  assumption is that previous state placed fabric src in the build directory
 echo "######## Testing chaincode ########"
