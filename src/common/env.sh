3ec4e56f170bbaa9d649010cd0a53cc258b184c4
Yorke Rhodes IV <Yorke.Rhodes.IV@ibm.com>
2018-07-06 16:54:21 -0400

    Fix environment variable default values
    Signed-off-by: Yorke Rhodes IV <Yorke.Rhodes.IV@ibm.com>

--- src/common/env.sh
+++ src/common/env.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
+ROOTDIR=${ROOTDIR:=$PWD}
 
-: "${ROOTDIR:='.'}"
-
-echo "ROOTDIR: " ${ROOTDIR}
-
-export GO_VERSION="1.9.2"
+export GO_VERSION=${GO_VERSION:="1.9.2"}
 
 # set location for go executables
 export GOROOT=${ROOTDIR}/go
 export PATH=${GOROOT}/bin:$PATH
 export GOPATH=${ROOTDIR}
 
-export HLF_VERSION="1.0.4"
+export HLF_VERSION=${HLF_VERSION:="1.0.4"}
 export FABRIC_SRC_DIR=${ROOTDIR}/fabric-${HLF_VERSION}
