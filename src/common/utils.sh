43f8192ee2ac91484872fed395de5764bed201c2
James Taylor <jamest@uk.ibm.com>
2018-07-10 14:08:30 +0100

    Add get_deploy_name utility function
    Include the new function in old style scripts to fix current
    problem if rest server hostname clashes. Also updates
    REST_SERVER_URLS env var format to work with vehicle
    manufacture sample.
    
    Closes #18
    
    Signed-off-by: James Taylor <jamest@uk.ibm.com>

--- src/common/utils.sh
+++ src/common/utils.sh
@@ -1,3 +1,21 @@
 #!/usr/bin/env bash
 
-# Common utility functions, e.g. to make curl requests
+# Creates a name that is likely to be unique, which is suitable for use
+# when deploying apps to bluemix
+#   get_deploy_name <uuid> [<name> ...]
+function get_deploy_name {
+    uuid="${1:?get_deploy_name must be called with at least one argument}"
+    shift
+
+    old_ifs="$IFS"
+    IFS='_'
+    name="$*"
+    IFS=$old_ifs
+
+    unique_name="${name}_${uuid}"
+
+    short_hash=$(echo "${unique_name}" | git hash-object --stdin | head -c 7)
+
+    deploy_name=$(echo "${name}" | head -c 43)${short_hash}
+    echo "${deploy_name}"
+}
