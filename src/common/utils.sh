d23a622a86a0394fec39b07089f17266f5eb1bde
Sanjay Ravindra <sanjay.ravindra@ibm.com>
2018-07-27 11:15:28 -0400

    Add documentation to utils.sh functions (#49)
    Add documentation to utils functions
    
    Signed-off-by: Sanjay Ravindra <sanjay.ravindra@ibm.com>

--- src/common/utils.sh
+++ src/common/utils.sh
@@ -2,10 +2,17 @@
 
 # Common utility functions, e.g. to make curl requests
 
-
-# Creates a name that is likely to be unique, which is suitable for use
+#######################################
+# Creates and displays a name that is likely to be unique and suitable for use
 # when deploying apps to bluemix
-#   get_deploy_name <uuid> [<name> ...]
+# Globals:
+#   None
+# Arguments:
+#   uuid: Universally unique identifier
+#   name: Additional name arguments
+# Returns:
+#   None
+#######################################
 function get_deploy_name {
     uuid="${1:?get_deploy_name must be called with at least one argument}"
     shift
@@ -23,12 +30,33 @@ function get_deploy_name {
     echo "${deploy_name}"
 }
 
+#######################################
+# Installs jq using curl and updates path
+# Globals:
+#   set: PATH
+# Arguments:
+#   None
+# Returns:
+#   None
+#######################################
 function install_jq {
     curl -o jq -L https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64
     chmod +x jq
     export PATH=${PATH}:${PWD}
 }
 
+#######################################
+# Performs curl request, displays response, and returns status
+# Globals:
+#   None
+# Arguments:
+#   $@: Curl options
+# Returns:
+#   err_no:
+#     1 = http status code is between 200 and 299 inclusive, indicating success
+#     0 = http status code is less than 200, indicating an informational response, or
+#         greater than 299, indicating redirection or error
+#######################################
 function do_curl {
     HTTP_RESPONSE=$(mktemp)
     HTTP_STATUS=$(curl -w '%{http_code}' -o "${HTTP_RESPONSE}" "$@")
@@ -38,7 +66,6 @@ function do_curl {
     then
         return 0
     else
-        # limit error response integer
         return 1
     fi
 }
