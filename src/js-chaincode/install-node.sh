3fb304cb29c74473463914aa39d4e62c5be3ded8
Francisco Taborda <taborda@gmail.com>
2018-08-01 23:26:42 +0100

    Fix install node script
    Signed-off-by: Francisco Taborda <taborda@gmail.com>

--- src/js-chaincode/install-node.sh
+++ src/js-chaincode/install-node.sh
@@ -5,13 +5,10 @@ export NVM_DIR=/home/pipeline/nvm
 
 mkdir -p $NVM_DIR
 
-
-
 echo "######## Installing Node.js version ${NODE_VERSION} using nvm ${NVM_VERSION} ########"
 npm config delete prefix \
-  && curl https://raw.githubusercontent.com/creationix/nvm/v${NVM_VERSION}/install.sh | sh \
+  && curl https://raw.githubusercontent.com/creationix/nvm/v${NVM_VERSION}/install.sh | bash \
   && . $NVM_DIR/nvm.sh \
-  && nvm install $NODE_VERSION \
   && nvm alias default $NODE_VERSION \
   && nvm use default \
   && node -v \
