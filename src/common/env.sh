d894fe519bc056d4e669700650beb6b86badf5b8
Francisco Taborda <taborda@gmail.com>
2018-08-01 17:46:00 +0100

    Add script to install node
    Signed-off-by: Francisco Taborda <taborda@gmail.com>

--- src/common/env.sh
+++ src/common/env.sh
@@ -3,6 +3,10 @@ ROOTDIR=${ROOTDIR:=$PWD}
 
 export GO_VERSION=${GO_VERSION:="1.9.2"}
 
+export NODE_VERSION=${NODE_VERSION:="10.7.0"}
+export NVM_VERSION=${NVM_VERSION:="0.33.11"}
+
+
 # set location for go executables
 export GOROOT=${ROOTDIR}/go
 export PATH=${GOROOT}/bin:$PATH
