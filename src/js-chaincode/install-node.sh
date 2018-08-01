257a55b2e323a67e204804917030c5eaa7d09dde
Francisco Taborda <taborda@gmail.com>
2018-08-01 18:01:42 +0100

    Create nvm folder on the node install script
    Signed-off-by: Francisco Taborda <taborda@gmail.com>

--- src/js-chaincode/install-node.sh
+++ src/js-chaincode/install-node.sh
@@ -3,6 +3,8 @@
 echo "######## Begin install and configure Node ########"
 export NVM_DIR=/home/pipeline/nvm
 
+mkdir -p $NVM_DIR
+
 echo "######## Installing Node.js version ${NODE_VERSION} using nvm ${NVM_VERSION} ########"
 npm config delete prefix \
   && curl https://raw.githubusercontent.com/creationix/nvm/v${NVM_VERSION}/install.sh | sh \
