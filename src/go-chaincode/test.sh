0b84c6d2a9279d95f518a8220b0ea2fa8da7ba2c
Ricardo Olivieri <r_olivieri@yahoo.com>
2018-11-04 17:30:50 -0500

    added assert package for go and test reporting tool
    Signed-off-by: Ricardo Olivieri <r_olivieri@yahoo.com>

--- src/go-chaincode/test.sh
+++ src/go-chaincode/test.sh
@@ -7,4 +7,10 @@ source "${SCRIPT_DIR}/common/env.sh"
 
 $DEBUG && set -x
 
-go test -v "chaincode/..." 
+# Install assert package for GO (needed for testing)
+go get github.com/stretchr/testify/assert
+# Install go-junit-report
+go get -u github.com/jstemmer/go-junit-report
+# Run test cases and send output/results to terminal and go-junit-report
+#go test -v "chaincode/..."
+go test -v "chaincode/..." 2>&1 | tee /dev/tty | $GOPATH/bin/go-junit-report > TEST-report.xml
