24a0ccbe117f93b2db9c5dc8d32c3d55d4dca417
Abinesh Sarvepalli <abisarvepalli@ibm.com>
2019-07-08 12:41:24 -0400

    log: print dirs at the end of build.sh

--- src/go-chaincode/build.sh
+++ src/go-chaincode/build.sh
@@ -7,6 +7,7 @@ source "${SCRIPT_DIR}/common/env.sh"
 
 $DEBUG && set -x
 
+
 echo "######## Download dependencies ########"
 
 # shellcheck source=src/go-chaincode/download-fabric.sh
@@ -15,16 +16,22 @@ source "${SCRIPT_DIR}/go-chaincode/download-fabric.sh"
 # shellcheck source=src/go-chaincode/install-go.sh
 source "${SCRIPT_DIR}/go-chaincode/install-go.sh"
 
+
+echo "######## Placing source in directory expected by go build ########"
+
 GOSOURCE="${GOPATH}/src"
 
 mkdir -p "${GOSOURCE}"
 cp -r "${CHAINCODEPATH}" "${GOSOURCE}/chaincode"
 
-echo "######## Placing source in directory expected by go build ########"
 # Let's put fabric source into gopath so that go can resolve dependencies with Fabric libraries 
-
 mkdir -p "${GOSOURCE}/github.com/hyperledger/"
 mv "${FABRIC_SRC_DIR}" "${GOSOURCE}/github.com/hyperledger/fabric"
+
+
+echo "######## Print current directories ########"
+ls
+
 
 # change to the correct path name \ path should be ./chaincode/go/example ??
 echo "######## Building chaincode ########"
