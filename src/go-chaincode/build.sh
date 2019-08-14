ac9307a16f4ebb43882ae31abfd1605f7a52313a
Abinesh Sarvepalli <abisarvepalli@ibm.com>
2019-08-14 14:51:56 -0400

    build go cc project
    Signed-off-by: Abinesh Sarvepalli <abisarvepalli@ibm.com>

--- src/go-chaincode/build.sh
+++ src/go-chaincode/build.sh
@@ -10,16 +10,19 @@ $DEBUG && set -x
 
 
 echo "######## Download dependencies ########"
+setup_env
+install_python $PYTHON_VERSION
 
+echo "=> download hfl-v${HLF_VERSION} source code"
 # shellcheck source=src/go-chaincode/download-fabric.sh
 source "${SCRIPT_DIR}/go-chaincode/download-fabric.sh"
 
+echo "=> install go binaries"
 # shellcheck source=src/go-chaincode/install-go.sh
 source "${SCRIPT_DIR}/go-chaincode/install-go.sh"
 
-install_node $NODE_VERSION $NVM_VERSION
-echo "npm config get prefix...$(npm config get prefix)"
-install_fabric-cli
+echo "=> install node v${NODE_VERSION} via nvm v${NVM_VERSION}"
+nvm_install_node $NODE_VERSION
 
 
 echo "######## Placing source in directory expected by go build ########"
@@ -34,16 +37,18 @@ mkdir -p "${GOSOURCE}/github.com/hyperledger/"
 mv "${FABRIC_SRC_DIR}" "${GOSOURCE}/github.com/hyperledger/fabric"
 
 
-echo "######## Print directories ########"
-echo "Current directory...$(pwd)"
-ls -a
-echo "Print env variables"
-env
-echo "Home directory...$HOME"
-ls -a $HOME
-
-
-
 # change to the correct path name \ path should be ./chaincode/go/example ??
 echo "######## Building chaincode ########"
-go build -v -x "chaincode/..."
+install_jq
+for org in $(cat ${CONFIGPATH} | jq -r 'keys | .[]'); do
+  for cc_path in $(cat ${CONFIGPATH} | jq -r ".${org}.chaincode | .[] | .path"); do    
+    cd ${GOSOURCE}/${cc_path}
+    go build -v -x $cc_path
+  done
+done
+cd ${ROOTDIR}
+# go build -v -x "chaincode/..."
+
+
+echo "######## Building fabric-cli ########"
+build_fabric_cli ${FABRIC_CLI_DIR}
