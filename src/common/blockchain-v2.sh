9ae16753a450396df8dcecf83c553e30bdc5d866
Abinesh Sarvepalli <abisarvepalli@ibm.com>
2019-08-14 14:48:55 -0400

    refactor: instantiate_cc func to use init-fn flag
    Signed-off-by: Abinesh Sarvepalli <abisarvepalli@ibm.com>

--- src/common/blockchain-v2.sh
+++ src/common/blockchain-v2.sh
@@ -1,46 +1,7 @@
 #!/usr/bin/env bash
 
 #######################################
-# Install chaincode provided specified parameters
-# Globals:
-#   None
-# Arguments:
-#   DEPLOY_CONFIG: JSON-formatted string specifying org installing
-# chaincode (reference README)
-#   PLATFORM: [ "node", "golang", "java" ]
-#   SRC_DIR: absolute path to root of chaincode source code
-# Returns:
-#   None
-#######################################
-function install_cc_auto {
-  local DEPLOY_CONFIG=$1
-  local PLATFORM=$2
-  local SRC_DIR=$3
-  # local ROOTDIR=${4:-"null"} #TEST:
-
-  for org in $(cat ${DEPLOY_CONFIG} | jq 'keys | .[]'); do
-    for ccindex in $(cat ${DEPLOY_CONFIG} | jq ".${org}.chaincode |  keys | .[]"); do
-      local cc=$(cat ${DEPLOY_CONFIG} | jq ".${org}.chaincode | .[${ccindex}]")      
-      for channel in $(echo ${cc} | jq ".channels | .[]"); do
-        local conn_profile="${SRC_DIR}/config/${org}-connprofile.json"
-        local admin_identity="${SRC_DIR}/config/${org}-admin.json"
-        local cc_name=$(cat ${SRC_DIR}/package.json | jq '.name')
-        local cc_version=$(cat ${SRC_DIR}/package.json | jq '.version')
-
-        # echo "conn_profile...${conn_profile//\"}"
-        # less ${conn_profile//\"}
-
-        local cmd="fabric-cli chaincode install --conn-profile ${conn_profile//\"} --org ${org//\"} --admin-identity ${admin_identity//\"} --cc-name ${cc_name//\"} --cc-version ${cc_version//\"} --cc-type ${PLATFORM//\"} --src-dir ${SRC_DIR//\"}"
-        echo $cmd
-        echo
-        echo $cmd | bash
-      done
-    done
-  done
-}
-
-#######################################
-# Instantiate chaincode on peer(s) provided specified parameters
+# Install chaincode on peer(s) provided specified parameters
 # Globals:
 #   None
 # Arguments:
@@ -70,56 +31,9 @@ function install_cc {
   echo ${cmd} | bash
 }
 
-#######################################
-# Instantiate chaincode on peer(s) provided specified parameters
-# Globals:
-#   None
-# Arguments:
-#   DEPLOY_CONFIG: JSON-formatted string specifying org installing chaincode (reference README)
-#   PLATFORM: [ "node", "golang", "java" ]
-#   SRC_DIR: absolute path to root of chaincode source code
-# Returns:
-#   None
-#######################################
-function instantiate_cc_auto {
-  # echo "still in development..."
-  local DEPLOY_CONFIG=$1
-  local PLATFORM=$2
-  local SRC_DIR=$3
-
-  for org in $(cat ${DEPLOY_CONFIG} | jq 'keys | .[]'); do
-    for ccindex in $(cat ${DEPLOY_CONFIG} | jq ".${org}.chaincode |  keys | .[]"); do
-      local cc=$(cat ${DEPLOY_CONFIG} | jq ".${org}.chaincode | .[${ccindex}]")
-      for channel in $(echo ${cc} | jq ".channels | .[]"); do
-        # echo ${channel}
-        # echo $(echo ${cc} | jq '.init_args')
-
-        # local conn_profile="$org-$channel-connprofile.json"
-        local conn_profile="${SRC_DIR}/config/${org}-connprofile.json"
-        local admin_identity="${SRC_DIR}/config/$org-admin.json"
-        local cc_name=$(cat ${SRC_DIR}/package.json | jq '.name')
-        local cc_version=$(cat ${SRC_DIR}/package.json | jq '.version')
-
-        local init_fn=$(echo ${cc} | jq '.init_fn')
-        local init_args=$(echo ${cc} | jq '.init_args')
-        # echo $init_args
-
-        # TEST: how to format multiple init_args into cli
-        if [[ -z $init_args || ${init_args} == [] ]]; then
-          local cmd="fabric-cli chaincode instantiate --conn-profile ${conn_profile//\"} --org ${org//\"} --admin-identity ${admin_identity//\"} --cc-name ${cc_name//\"} --cc-version ${cc_version//\"} --cc-type ${PLATFORM//\"} --channel ${channel//\"} --init-function ${init_fn//\"}"
-        else
-          local cmd="fabric-cli chaincode instantiate --conn-profile ${conn_profile//\"} --org ${org//\"} --admin-identity ${admin_identity//\"} --cc-name ${cc_name//\"} --cc-version ${cc_version//\"} --cc-type ${PLATFORM//\"} --channel ${channel//\"} --init-function ${init_fn//\"} --init-args \"${init_args//\"}\""
-        fi
-        echo ${cmd}; echo; echo
-
-        echo ${cmd} | bash
-      done
-    done
-  done
-}
 
 #######################################
-# Invoke chaincode function provided specified parameters
+# Instantiate chaincode function provided specified parameters
 # Globals:
 #   None
 # Arguments:
@@ -146,57 +60,13 @@ function instantiate_cc {
   local init_fn=${8:-"init"}
   local init_args=${9:-"[]"}
 
-  local cmd="fabric-cli chaincode instantiate --conn-profile ${conn_profile//\"} --org ${org//\"} --admin-identity ${admin_identity//\"} --cc-name ${cc_name//\"} --cc-version ${cc_version//\"} --cc-type ${platform//\"} --channel ${channel//\"} --init-function ${init_fn//\"} --init-args ${init_args//\"}"
+  local cmd="fabric-cli chaincode instantiate --conn-profile ${conn_profile//\"} --org ${org//\"} --admin-identity ${admin_identity//\"} --cc-name ${cc_name//\"} --cc-version ${cc_version//\"} --cc-type ${platform//\"} --channel ${channel//\"} --init-fn ${init_fn//\"} --init-args ${init_args//\"}"
 
   echo ${cmd}
   echo
   echo ${cmd} | bash
 }
 
-#######################################
-# Invoke chaincode function provided specified parameters
-# Globals:
-#   None
-# Arguments:
-#   DEPLOY_CONFIG: JSON-formatted string specifying org installing chaincode (reference README)
-#   PLATFORM: [ "node", "golang", "java" ]
-#   SRC_DIR: absolute path to root of chaincode source code
-# Returns:
-#   None
-#######################################
-function invoke_cc_auto {
-  local DEPLOY_CONFIG=$1
-  local PLATFORM=$2
-  local SRC_DIR=$3
-
-  for org in $(cat ${DEPLOY_CONFIG} | jq 'keys | .[]'); do
-    for ccindex in $(cat ${DEPLOY_CONFIG} | jq ".${org}.chaincode |  keys | .[]"); do
-      local cc=$(cat ${DEPLOY_CONFIG} | jq ".${org}.chaincode | .[${ccindex}]")
-      for channel in $(echo ${cc} | jq ".channels | .[]"); do
-        # local conn_profile="$org-$channel-connprofile.json"
-        local conn_profile="${SRC_DIR}/config/${org}-connprofile.json"
-        local admin_identity="${SRC_DIR}/config/$org-admin.json"
-        local cc_name=$(cat ${SRC_DIR}/package.json | jq '.name')
-        local cc_version=$(cat ${SRC_DIR}/package.json | jq '.version')
-
-        local invoke_fn=$(echo ${cc} | jq '.init_fn')
-        local invoke_args=$(echo ${cc} | jq '.init_args')
-        # echo $init_args
-
-        # TEST: how to format multiple init_args into cli
-        if [[ -z $invoke_args || ${invoke_args} == "[]" ]]; then
-          local cmd="fabric-cli chaincode invoke --conn-profile ${conn_profile//\"} --org ${org//\"} --admin-identity ${admin_identity//\"} --cc-name ${cc_name//\"} --channel ${channel//\"} --invoke-fn ${invoke_fn//\"} --query true"
-        else
-          local cmd="fabric-cli chaincode invoke --conn-profile ${conn_profile//\"} --org ${org//\"} --admin-identity ${admin_identity//\"} --cc-name ${cc_name//\"} --cc-version ${cc_version//\"} --cc-type ${PLATFORM//\"} --channel ${channel//\"} --invoke-fn ${invoke_fn//\"} --invoke-args ${invoke_args//\"} --query true"
-        fi
-
-        echo "${cmd}"
-        echo
-        echo "${cmd}" | bash
-      done
-    done
-  done
-}
 
 #######################################
 # Invoke chaincode function provided specified parameters
@@ -215,6 +85,7 @@ function invoke_cc_auto {
 # Returns:
 #   None
 #######################################
+#TODO: Properly integrate invoke_cc
 function invoke_cc {
   local org=$1
   local admin_identity=$2
@@ -226,9 +97,9 @@ function invoke_cc {
   local invoke_fn=${8:-"queryAllCars"}
   local invoke_args=${9:-"[]"}
 
-  local cmd="fabric-cli chaincode instantiate --conn-profile ${conn_profile//\"} --org ${org//\"} --admin-identity ${admin_identity//\"} --cc-name ${cc_name//\"} --cc-version ${cc_version//\"} --cc-type ${platform//\"} --channel ${channel//\"} --invoke-fn ${invoke_fn//\"} --invoke-args ${invoke_args//\"}"
+  local cmd="fabric-cli chaincode invoke --conn-profile ${conn_profile//\"} --org ${org//\"} --admin-identity ${admin_identity//\"} --cc-name ${cc_name//\"} --cc-version ${cc_version//\"} --cc-type ${platform//\"} --channel ${channel//\"} --invoke-fn ${invoke_fn//\"} --invoke-args ${invoke_args//\"}"
 
   echo ${cmd}
   echo
-  echo ${cmd} | bash
+  # echo ${cmd} | bash
 }
