0643680c0d586b9b4e92d92341db6010eaa4d154
Abinesh Sarvepalli <abisarvepalli@ibm.com>
2019-08-19 13:45:02 -0400

    working golang deploy+test

--- src/go-chaincode/deploy.sh
+++ src/go-chaincode/deploy.sh
@@ -7,7 +7,8 @@ source "${SCRIPT_DIR}/common/env.sh"
 # shellcheck source=src/common/utils.sh
 source "${SCRIPT_DIR}/common/utils.sh"
 # shellcheck source=src/common/blockchain.sh
-source "${SCRIPT_DIR}/common/blockchain.sh"
+source "${SCRIPT_DIR}/common/blockchain-v2.sh"
+
 source "${SCRIPT_DIR}/go-chaincode/vendor-dependencies.sh"
 
 $DEBUG && set -x
@@ -17,8 +18,63 @@ if [[ ! -f $CONFIGPATH ]]; then
   exit 1
 fi
 
+echo "######## Download dependencies ########"
+nvm_install_node $NODE_VERSION
 install_jq
-setup_service_constants
-provision_blockchain
-fetch_dependencies $CONFIGPATH
-deploy_fabric_chaincode golang $CONFIGPATH
+
+echo "######## Link fabric-cli ########"
+build_fabric_cli $FABRIC_CLI_DIR
+
+echo "=> Validating dependencies..."
+if [[ ! -n $(command -v fabric-cli) ]]; then
+  error_exit "fabric-cli interface not found in PATH env variable"  
+fi
+if [[ ! -n $(command -v jq) ]]; then
+  error_exit "jq interface not found in PATH env variable"
+fi
+
+for org in $(cat ${CONFIGPATH} | jq 'keys | .[]'); do
+  for ccindex in $(cat ${CONFIGPATH} | jq ".${org}.chaincode | keys | .[]"); do
+    cc=$(cat ${CONFIGPATH} | jq ".${org}.chaincode | .[${ccindex}]")
+    for channel in $(echo ${cc} | jq '.channels | .[]'); do
+      conn_profile="$(pwd)/config/${org}-${channel}.json"
+      admin_identity="$(pwd)/config/${org}-admin.json"
+      cc_name=$(echo ${cc} | jq -r '.name')
+      cc_version=$(echo ${cc} | jq -r '.version')
+      cc_src=$(echo ${cc} | jq -r '.path')  
+
+      # check chaincode path exists in GOPATH/src directory
+      echo "-> reviewing chaincode path...${cc_src}"
+      ls ${GOPATH}/src/${cc_src}
+      if [[! -n $(ls ${GOPATH}/src/${cc_src}) ]]; then
+        error_exit "cannot locate chaincode path for ${cc_src}"
+      fi
+
+      # should install
+      if [[ "true" == $(cat ${CONFIGPATH} | jq ".${org}.chaincode | .[${ccindex}] | .install") ]]; then
+        echo "=> installing...${cc_src}"        
+        retry_with_backoff 5 install_cc "${org}" "${admin_identity}" "${conn_profile}" "${cc_name}" "${cc_version}" "golang" ${cc_src}
+      fi
+
+      # should instantiate
+      if [[ "true" == $(cat ${CONFIGPATH} | jq ".${org}.chaincode | .[${ccindex}] | .instantiate") ]]; then
+        init_fn=$(cat $CONFIGPATH | jq ".${org}.chaincode | .[${ccindex}] | .init_fn?")
+        if [[ -z $init_fn ]]; then init_fn="init"; fi
+
+        init_args=$(cat $CONFIGPATH | jq ".${org}.chaincode | .[${ccindex}] | .init_args?")
+        if [[ -z $init_args ]]; then init_args="[]"; fi
+
+        echo "=> instantiating...${cc_src}"
+        retry_with_backoff 5 instantiate_cc "${org}" "${admin_identity}" "${conn_profile}" "${cc_name}" "${cc_version}" "${channel}" "golang" "${init_fn}" "${init_args}"
+
+        # # test invokation of init method
+        # invoke_cc $org $admin_identity
+      fi      
+    done
+  done
+done
+
+# setup_service_constants
+# provision_blockchain
+# fetch_dependencies $CONFIGPATH
+# deploy_fabric_chaincode golang $CONFIGPATH
