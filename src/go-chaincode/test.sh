a7eed4d087d7c7b9bdddfd26fb57092b7190961d
Abinesh Sarvepalli <abisarvepalli@ibm.com>
2019-08-21 12:37:59 -0400

    cleanup code

--- src/go-chaincode/test.sh
+++ src/go-chaincode/test.sh
@@ -16,7 +16,6 @@ $DEBUG && set -x
 
 # Install assert package for GO (needed for testing)
 echo "=> go get assert"
-go version
 go get github.com/stretchr/testify/assert
 
 # Install go-junit-report
@@ -24,19 +23,11 @@ echo "=> go get go-junit-report"
 go get -u github.com/jstemmer/go-junit-report
 
 # Run test cases and send results to go-junit-report
-#go test -v "chaincode/..."
 echo '=> check $GOPATH/src/chaincode directory'
 ls "$GOPATH/src/chaincode"
 
 
-# echo "=> go test -v chaincode"
 # TODO: 
 # - try running just go test
 go test -v "chaincode/..." 2>&1 | tee tst-output.txt | go-junit-report > TEST-report.xml
-# go test -v "chaincode/..."
-
-# echo "=> ls output from test"
-# ls | grep tst-output.txt
-# ls | grep TEST-report.xml
-# go test -v "local/ping-cc/..." 2>&1 | tee tst-output.txt | go-junit-report > TEST-report.xml
 cat tst-output.txt
