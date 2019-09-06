ebfe197c55bbd7f1628295e413ae86055194b9f7
Abinesh Sarvepalli <abisarvepalli@ibm.com>
2019-09-06 10:32:08 -0400

    upd+log: install python from python releases and standardize logs
    Signed-off-by: Abinesh Sarvepalli <abisarvepalli@ibm.com>

--- src/js-chaincode/build.sh
+++ src/js-chaincode/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "======== Build chaincode ========"
+echo "######## Build chaincode ########"
 
 # shellcheck source=src/common/env.sh
 source "${SCRIPT_DIR}/common/env.sh"
@@ -9,18 +9,19 @@ source "${SCRIPT_DIR}/common/utils.sh"
 
 $DEBUG && set -x
 
-echo "######## Download dependencies ########"
+echo "======== Download dependencies ========"
 setup_env
 install_python $PYTHON_VERSION
+# echo "Y" | apt-get install python2.7
 nvm_install_node ${NODE_VERSION}
 
-echo "######## Building fabric-cli tool ########"
+echo "======== Building fabric-cli tool ========"
 cd ${FABRIC_CLI_DIR}
 npm install
 npm run build
 npm link #?
 
-echo "######## Building chaincode ########"
+echo "======== Building chaincode ========"
 cd ${CHAINCODEPATH}
 npm install
 npm run build # transpile from typescript to javascript
