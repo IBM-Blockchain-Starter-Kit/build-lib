7e3e13bd3bd7cbd3e0a9e724b6bc8e434308c02c
Abinesh Sarvepalli <abisarvepalli@ibm.com>
2019-09-09 16:17:50 -0400

    LOG clean logs

--- src/common/blockchain.sh
+++ src/common/blockchain.sh
@@ -15,7 +15,7 @@
 # Returns:
 #   None
 #######################################
-function install_cc {
+function install_fabric_chaincode {
   local ORG=$1
   local ADMIN_IDENTITY=$2
   local CONN_PROFILE=$3
@@ -24,14 +24,12 @@ function install_cc {
   local PLATFORM=$6
   local SRC_DIR=$7
 
-  CMD="fabric-cli chaincode install --conn-profile ${CONN_PROFILE} --org ${ORG} \
+  local CMD="fabric-cli chaincode install --conn-profile ${CONN_PROFILE} --org ${ORG} \
   --admin-identity ${ADMIN_IDENTITY} --cc-name ${CC_NAME} --cc-version ${CC_VERSION} \
   --cc-type ${PLATFORM} --src-dir ${SRC_DIR}"
 
-  echo
-  echo ">>>" ${CMD}
-  echo
-  echo ${CMD} | bash
+  echo ">>>" ${CMD}; echo
+  echo ${CMD} | bash; echo
 }
 
 
@@ -53,7 +51,7 @@ function install_cc {
 # Returns:
 #   None
 #######################################
-function instantiate_cc {
+function instantiate_fabric_chaincode {
   local ORG=$1
   local ADMIN_IDENTITY=$2
   local CONN_PROFILE=$3
@@ -79,46 +77,6 @@ function instantiate_cc {
     local COLLECTIONS_CONFIG_FLAG=" --collections-config $(pwd)/${COLLECTIONS_CONFIG}"
   fi
 
-  echo 
-  echo ">>>" ${CMD} ${INIT_FN_FLAG:-""} ${INIT_ARGS_FLAG:-""} ${COLLECTIONS_CONFIG_FLAG:-""} "--timeout 360000"
-  echo
-  echo ${CMD} ${INIT_FN_FLAG:-""} ${INIT_ARGS_FLAG:-""} ${COLLECTIONS_CONFIG_FLAG:-""} "--timeout 360000" | bash
-}
-
-
-#######################################
-# Invoke chaincode function provided specified parameters
-# Globals:
-#   None
-# Arguments:
-#   -  $1: ORG: org msp name
-#   -  $2: ADMIN_IDENTITY: abs path to associated admin identity
-#   -  $3: CONN_PROFILE: abs path to the connection profile
-#   -  $4: CC_NAME: chaincode name to be instantiated on the channel
-#   -  $5: CC_VERSION: chaincode version to be instantiated on the channel
-#   -  $6: CHANNEL: channel name that chaincode will be instantiated on
-#   -  $7: PLATFORM: [ golang, node, java ]
-#  (-) $8: INVOKE_FN: name of function to invoke
-#  (-) $9: INVOKE_ARGS: args passef into the invoke function
-# Returns:
-#   None
-#######################################
-function invoke_cc {
-  local ORG=$1
-  local ADMIN_IDENTITY=$2
-  local CONN_PROFILE=$3
-  local CC_NAME=$4
-  local CC_VERSION=$5
-  local CHANNEL=$6
-  local PLATFORM=$7
-  local INVOKE_FN=${8:-""}
-  local INVOKE_ARGS=${9:-""}  
-
-  local CMD="fabric-cli chaincode invoke --conn-profile ${CONN_PROFILE} --org ${ORG} \
-  --admin-identity ${ADMIN_IDENTITY} --cc-name ${CC_NAME} --cc-version ${CC_VERSION} \
-  --cc-type ${PLATFORM} --channel ${CHANNEL} --invoke-fn ${INVOKE_FN} --invoke-args ${INVOKE_ARGS}"
-
-  echo ">>>" ${CMD}
-  echo
-  echo ${CMD} | bash
+  echo ">>>" ${CMD} ${INIT_FN_FLAG:-""} ${INIT_ARGS_FLAG:-""} ${COLLECTIONS_CONFIG_FLAG:-""} "--timeout 360000"; echo
+  echo ${CMD} ${INIT_FN_FLAG:-""} ${INIT_ARGS_FLAG:-""} ${COLLECTIONS_CONFIG_FLAG:-""} "--timeout 360000" | bash; echo
 }
