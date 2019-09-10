d95dad95dfe7b9f32a8aa3c7599702c0b6bbdbfa
Abinesh Sarvepalli <abisarvepalli@ibm.com>
2019-09-10 15:58:55 -0400

    UPD deploy to multiple channels sourcing from single org

--- src/js-chaincode/test.sh
+++ src/js-chaincode/test.sh
@@ -9,12 +9,27 @@ source "${SCRIPT_DIR}/common/utils.sh"
 source "${SCRIPT_DIR}/common/blockchain.sh"
 
 $DEBUG && set -x
+cd $CHAINCODEPATH
 
 echo "======== Download dependencies ========"
-cd $CHAINCODEPATH
 nvm_install_node "$NODE_VERSION"
-# npm install
-# npm run build
+install_jq
 
 echo "======== Run cc tests ========"
 npm run test
+
+echo "======== Run deploy_config.json tests ========"
+# convert strings into array
+if [[ ${ADMIN_IDENTITY_STRING::1} == "[" ]]; then
+    # check same number of identities + profiles defined
+    hm_adminids=$(echo $ADMIN_IDENTITY_STRING | jq "keys | length")
+    hm_connprofs=$(echo $CONNECTION_PROFILE_STRING | jq "keys | length")
+    if [[ $hm_adminids != $hm_connprofs ]]; then
+        error_exit "number of ADMIN IDENTITIES does not match the number of CONNECTION PROFILES"
+    fi
+
+    hm_orgs=$(cat $CONFIGPATH | jq "keys | length")
+    if [[ $hm_adminids != $hm_orgs ]]; then
+        error_exit "number of ADMIN IDENTITIES does not match the number of ORGANIZATIONS defined in the deploy_config.json file"    
+    fi
+fi
