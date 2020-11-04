30968dbaabfe6d1dc49de0f7a120ddc05f80f579
Huy Tran <httran8884@gmail.com>
2020-11-04 14:58:17 -0600

    remove cacert, dont need it

--- src/common/create_msp_from_identity.sh
+++ src/common/create_msp_from_identity.sh
@@ -13,26 +13,26 @@ fi
 ## Check if dir exists
 [[ ! -d "$1" ]] && mkdir "$1"
 
-mkdir -p "${1}/msp/cacerts" && \
-    mkdir -p "${1}/msp/keystore" && \
-    mkdir -p "${1}/msp/signcerts" && \
-    mkdir -p "${1}/msp/admincerts"
+#mkdir -p "${1}/msp/cacerts" && \
+mkdir -p "${1}/msp/keystore" && \
+mkdir -p "${1}/msp/signcerts" && \
+mkdir -p "${1}/msp/admincerts"
 
 cert=$2
-ca=$3
-key=$4
-name=$5
+#ca=$3
+key=$3
+name=$4
 
 if [[ "$cert" == "null"  ]];then
     fatalln "cert from json not found"
-elif [[ "$ca" == "null"  ]];then
-    fatalln "ca from json not found"
+#elif [[ "$ca" == "null"  ]];then
+#    fatalln "ca from json not found"
 elif [[ "$key" == "null" ]];then
     fatalln "key from json not found"
 fi
 
 echo "${cert}" | base64 -d > "${1}/msp/signcerts/cert.pem"
 echo "${cert}" | base64 -d > "${1}/msp/admincerts/cert.pem"
-echo "${ca}" | base64 -d > "${1}/msp/cacerts/${name//[[:blank:]]/}.pem"
+#echo "${ca}" | base64 -d > "${1}/msp/cacerts/${name//[[:blank:]]/}.pem"
 SIGNING_ID=$(openssl x509 -noout -fingerprint -sha256 -inform pem -in "${1}/msp/signcerts/cert.pem" | sed 's/SHA256 Fingerprint=//g' | sed 's/://g' | tr  '[:upper:]' '[:lower:]')
 echo "${key}" | base64 -d > "${1}/msp/keystore/${SIGNING_ID}_sk"
