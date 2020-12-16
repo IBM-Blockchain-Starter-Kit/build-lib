af7a18b1f5d57e7740a046ec62a31f3dfff8f7de
Huy Tran <httran8884@gmail.com>
2020-12-16 10:31:16 -0600

    remove version failutre

--- src/js-chaincode/package.sh
+++ src/js-chaincode/package.sh
@@ -31,9 +31,9 @@ echo "LATEST_SEQ=${LATEST_SEQ}" >> build.properties
 export CC_SEQUENCE=${CC_SEQUENCE_OVERRIDE:-$(expr $LATEST_SEQ + 1)}
 
 ## Update package.json cc name and version
-cd "${CC_REPO_DIR}"
-npm version "${CC_VERSION}"
-cd -
+#cd "${CC_REPO_DIR}"
+#npm version "${CC_VERSION}"
+#cd -
 
 packageCC "${CC_REPO_DIR}" "${CC_NAME}" "${CC_VERSION}" "${CC_SEQUENCE}" "node"
 
