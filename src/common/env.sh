016f399b5ac86350913f386831b7139a1a630543
huytran1ibm <36869410+huytran1ibm@users.noreply.github.com>
2021-05-10 09:54:46 -0500

    Update env.sh

--- src/common/env.sh
+++ src/common/env.sh
@@ -124,7 +124,7 @@ if [[ $HLF_VERSION == "1."* && $ENABLE_PEER_CLI == 'true' ]] || [[ $HLF_VERSION 
 
     # Note: chaincode level shouldn't be array as deploy_config.json is a specific chaincode configuration for distinct source code deployment
     # TODO Allow CC_NAME override at pipeline
-    if [[ ! -z "${CC_INDEX}" ]];then
+    if [[ -z "${CC_INDEX}" ]];then
         #set default
         CC_INDEX=0
     fi
